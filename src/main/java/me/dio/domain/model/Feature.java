c0504957256725a227a041f3d38b6c55bf6cbc04
Daniel Nascimento Tomaz <danielsabio1000@hotmail.com>
2025-07-25 07:52:15 -0300

    Criação da classes do sistema

---
+++ src/main/java/me/dio/domain/model/Feature.java
@@ -0,0 +1,41 @@
+package me.dio.domain.model;
+
+public class Feature {
+    private Long id;
+    private  String icon;
+    private String description;
+
+    public Feature() {
+    }
+
+
+    public Feature(Long id, String icon, String description) {
+        this.id = id;
+        this.icon = icon;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/me/dio/domain/model/User.java
@@ -0,0 +1,79 @@
+package me.dio.domain.model;
+
+import java.util.List;
+
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    private Account account;
+
+    private List<Feature> features;
+
+    private Card card;
+
+    private List<News> news;
+
+    public User() {
+    }
+
+    public User(Long id, String name, Account account, List<Feature> features, Card card, List<News> news) {
+        this.id = id;
+        this.name = name;
+        this.account = account;
+        this.features = features;
+        this.card = card;
+        this.news = news;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
+}
